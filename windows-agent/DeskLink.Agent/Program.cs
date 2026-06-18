6a61066dc6698f46e0db2268490939f47e1739b2
AvanishRD <avanishreddydr@gmail.com>
2026-06-19 00:09:26 +0530

    Add system details to status endpoint

--- windows-agent/DeskLink.Agent/Program.cs
+++ windows-agent/DeskLink.Agent/Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -5,12 +7,24 @@ app.MapGet("/", () => "DeskLink Agent is running");
 
 app.MapGet("/status", () =>
 {
+    var machineName = Environment.MachineName;
+    var userName = Environment.UserName;
+    var osVersion = Environment.OSVersion.ToString();
+
+    var totalMemory = GC.GetGCMemoryInfo().TotalAvailableMemoryBytes;
+
     return Results.Ok(new
     {
         app = "DeskLink.Agent",
         status = "online",
-        device = Environment.MachineName,
-        time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
+        device = machineName,
+        user = userName,
+        os = osVersion,
+        time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+        memory = new
+        {
+            totalAvailableBytes = totalMemory
+        }
     });
 });
 
